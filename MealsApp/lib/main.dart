ba20c279f17db53219e12d47af95e40a674f4393
flukerbooker <win.vorapol52@gmail.com>
2021-01-12 18:59:03 +0700

    [UPDATE] Style & Theming

--- MealsApp/lib/main.dart
+++ MealsApp/lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:MealsApp/categories_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,28 +13,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('DeliMeals'),
-      ),
-      body: Center(
-        child: Text('Navigation Time!'),
-      ),
+          primarySwatch: Colors.pink,
+          accentColor: Colors.amber,
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          fontFamily: 'Raleway',
+          textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              bodyText2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              headline6: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'RobotoCondensed',
+                  fontWeight: FontWeight.bold))),
+      home: CategoriesScreen(),
     );
   }
 }
