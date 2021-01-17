b49655a1fb967e68a6366d28e31212a43881abe9
flukerbooker <win.vorapol52@gmail.com>
2021-01-17 15:47:31 +0700

    [ADD] Custom Drawer

--- MealsApp/lib/screens/tabs_screen.dart
+++ MealsApp/lib/screens/tabs_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import './favorite_screen.dart';
 import './categories_screen.dart';
+import '../widgets/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   @override
@@ -26,6 +27,7 @@ class _TabsScreenState extends State<TabsScreen> {
       appBar: AppBar(
         title: Text(_pages[_selectedPageIndex]['title']),
       ),
+      drawer: MainDrawer(),
       body: _pages[_selectedPageIndex]['page'],
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
