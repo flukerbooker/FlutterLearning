93b7ced0aff1985861a0b6c1d2e89814e2b548e4
flukerbooker <win.vorapol52@gmail.com>
2021-01-30 12:09:12 +0700

    [UPDATE] Favorite Toggle with Provider

--- ShopApp/lib/widgets/products_grid.dart
+++ ShopApp/lib/widgets/products_grid.dart
@@ -12,8 +12,10 @@ class ProductsGrid extends StatelessWidget {
     return GridView.builder(
       padding: const EdgeInsets.all(10.0),
       itemCount: products.length,
-      itemBuilder: (ctx, index) => ProductItem(
-          products[index].id, products[index].title, products[index].imageUrl),
+      itemBuilder: (ctx, index) => ChangeNotifierProvider(
+        create: (c) => products[index],
+        child: ProductItem(),
+      ),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3 / 2,
