698b4fb91abad2739d7604bff80a97a2af1ed18b
flukerbooker <win.vorapol52@gmail.com>
2021-02-03 00:50:50 +0700

    [UPDATE] Orders Screen with Future Builder

--- ShopApp/lib/providers/orders.dart
+++ ShopApp/lib/providers/orders.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 import './cart.dart';
 
@@ -26,41 +26,61 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
+  Future<void> fetchAndSetOrders() async {
+    const url =
+        'https://flutterlearning-755cb-default-rtdb.firebaseio.com/orders.json';
+    final response = await http.get(url);
+    final List<OrderItem> loadedOrders = [];
+    final extractedData = json.decode(response.body) as Map<String, dynamic>;
+    if (extractedData == null) {
+      return;
+    }
+    extractedData.forEach((orderId, orderData) {
+      loadedOrders.add(OrderItem(
+        id: orderId,
+        amount: orderData['amount'],
+        products: (orderData['products'] as List<dynamic>)
+            .map((item) => CartItem(
+                id: item['id'],
+                title: item['title'],
+                quantity: item['quantity'],
+                price: item['price']))
+            .toList(),
+        dateTime: DateTime.parse(orderData['dateTime']),
+      ));
+    });
+    _orders = loadedOrders.reversed.toList();
+    notifyListeners();
+  }
+
   Future<void> addOrder(List<CartItem> cartProducts, double total) async {
-    final url =
+    const url =
         'https://flutterlearning-755cb-default-rtdb.firebaseio.com/orders.json';
     final timestamp = DateTime.now();
-    try {
-      final response = await http.post(
-        url,
-        body: json.encode(
-          {
-            'amount': total,
-            'dataTime': timestamp.toIso8601String(),
-            'product': cartProducts
-                .map((cp) => {
-                      'id': cp.id,
-                      'title': cp.title,
-                      'price': cp.price,
-                      'quantity': cp.quantity,
-                    })
-                .toList(),
-          },
-        ),
-      );
-      _orders.insert(
-        0,
-        OrderItem(
-          id: json.decode(response.body)['name'],
-          amount: total,
-          dateTime: timestamp,
-          products: cartProducts,
-        ),
-      );
-      notifyListeners();
-    } catch (error) {
-      print(error);
-      throw error;
-    }
+    final response = await http.post(
+      url,
+      body: json.encode({
+        'amount': total,
+        'dateTime': timestamp.toIso8601String(),
+        'products': cartProducts
+            .map((cp) => {
+                  'id': cp.id,
+                  'title': cp.title,
+                  'quantity': cp.quantity,
+                  'price': cp.price,
+                })
+            .toList(),
+      }),
+    );
+    _orders.insert(
+      0,
+      OrderItem(
+        id: json.decode(response.body)['name'],
+        amount: total,
+        dateTime: timestamp,
+        products: cartProducts,
+      ),
+    );
+    notifyListeners();
   }
 }
--- ShopApp/lib/providers/products_provider.dart
+++ ShopApp/lib/providers/products_provider.dart
@@ -26,6 +26,9 @@ class ProductsProvider with ChangeNotifier {
     try {
       final response = await http.get(url);
       final extractedData = json.decode(response.body) as Map<String, dynamic>;
+      if (extractedData == null) {
+        return;
+      }
       final List<Product> loadedProducts = [];
       extractedData.forEach((prodId, prodData) {
         loadedProducts.add(Product(
