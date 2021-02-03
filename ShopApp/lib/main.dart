a28fa7faf6d7d3a0716461d21b01f97671053674
flukerbooker <win.vorapol52@gmail.com>
2021-02-03 20:05:49 +0700

    [ADD] Auth Screen

--- ShopApp/lib/main.dart
+++ ShopApp/lib/main.dart
@@ -9,6 +9,7 @@ import './providers/orders.dart';
 import './screens/orders_screen.dart';
 import './screens/user_products_screen.dart';
 import './screens/edit_product_screen.dart';
+import './screens/auth_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -35,7 +36,7 @@ class MyApp extends StatelessWidget {
             fontFamily: 'Lato',
             visualDensity: VisualDensity.adaptivePlatformDensity,
           ),
-          home: ProductOverviewScreen(),
+          home: AuthScreen(),
           routes: {
             ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
             CartScreen.routeName: (ctx) => CartScreen(),
